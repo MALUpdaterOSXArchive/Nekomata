2be6ed7796663d2964a701f0e5d0ef768bc305c9
James Moy <chikorita157@chikorita157.com>
2018-07-22 16:15:48 -0400

    Add crash reporting

--- Nekomata/Program.cs
+++ Nekomata/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CrashReporterDotNET;
 
 namespace Nekomata
 {
@@ -14,9 +15,35 @@ namespace Nekomata
         [STAThread]
         static void Main()
         {
+            Application.ThreadException += ApplicationThreadException;
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWindow());
+            
+        }
+
+        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+        {
+            SendReport((Exception)unhandledExceptionEventArgs.ExceptionObject);
+            Environment.Exit(0);
+        }
+
+        private static void ApplicationThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            SendReport(e.Exception);
+        }
+
+        public static void SendReport(Exception exception, string developerMessage = "")
+        {
+            var reportCrash = new ReportCrash("Email where you want to receive crash reports.")
+            {
+                DeveloperMessage = developerMessage
+            };
+
+            reportCrash.Send(exception);
         }
     }
 }
